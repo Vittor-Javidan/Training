5eb73e5af48831cf4d5739bdbc16a78d41cab33f
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-02-11 23:24:39 -0300

    app.js UPDATE: connectivity to mongoDB. NotFound route and errorHandler middlewares implemented

--- NodeJs/Express/Store_API-Working_in_Progress/app.js
+++ NodeJs/Express/Store_API-Working_in_Progress/app.js
@@ -3,3 +3,39 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const connectDB = require('./db/connect')
+
+//middlewares require
+const notFoundMiddleware = require('./middleware/not_found')
+const errorHandlerMiddleware = require('./middleware/error_handler')
+app.use(express.json())
+
+//routes
+app.get('/', (req, res) => {
+    res.send(`
+        <h1>Store API</h1>
+        <a href="/api/v1/products">products</a>
+    `)
+})
+
+//Porducts route
+
+
+
+
+//end middlewares
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
+const port = process.env.PORT || 5000
+const start = async () =>{
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`Server is listening on port ${port}`))
+    } catch (error) {
+        
+    }
+}
+
+start()
