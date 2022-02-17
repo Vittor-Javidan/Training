ba06a16aa15e05f4f73efd9148314e0a731b3c85
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-02-17 20:31:57 -0300

    React_Course_Begginers: 5_Thought_Experiment

---
+++ React/YT_Courses/React_Course_Begginers/5_Thought_Experiment/index.js
@@ -0,0 +1,19 @@
+const page = (
+   <div>
+      <h1>My awesome website in React</h1>
+      <h3>Reasons I love React</h3>
+      <ol>
+         <li>It's composable</li>
+         <li>It's declarative</li>
+         <li>It's a hireable skill</li>
+         <li>It's a actively mainteined by skilled people</li>
+      </ol>
+   </div>
+)
+
+//document.getElementById("root").append(JSON.stringify(page))
+
+ReactDOM.render(
+   page,
+   document.getElementById('root')
+)
