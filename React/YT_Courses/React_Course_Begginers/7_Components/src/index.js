303592ba4b31de09497177a09cdd5cdaaa878c23
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-02-18 14:26:14 -0300

    React_Course_Begginers: 7_Components refactored

--- React/YT_Courses/React_Course_Begginers/7_Components/src/index.js
+++ React/YT_Courses/React_Course_Begginers/7_Components/src/index.js
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom"
 import Header from "./Components/Header"
 import Content from "./Components/Content"
 import Footer from "./Components/Footer"
+import "./index.css"
 
 function Page() {
 
