64f25fa13fa75c26e5e208edc934020ac32f0ce2
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-02-25 04:32:01 -0300

    React/Projects/Tenzies_Game: win and restart system implemented

--- React/Projects/Tenzies_Game/app/src/App.js
+++ React/Projects/Tenzies_Game/app/src/App.js
@@ -1,6 +1,7 @@
 import React from "react"
 import Dice from "./Components/Dice"
 import { nanoid } from "nanoid"
+import Confetti from 'react-confetti'
 
 function generateNewDices(diceAmount) {
    const newDiceObjects = []
@@ -15,29 +16,47 @@ function generateNewDices(diceAmount) {
    return newDiceObjects
 }
 
-function rollDice(event, diceAmount, setDiceObjects) {
+function rollDice(event, diceAmount, setDiceObjects, tenzies) {
    event.stopPropagation()
-   setDiceObjects(oldObjects => {
-      const newDices = generateNewDices(diceAmount)
-      for(let i = 0; i < oldObjects.length; i++){
-         oldObjects[i].isSelected && (newDices[i] = oldObjects[i]) 
-      }
-      return newDices
-   })
+   if(!tenzies){
+      setDiceObjects(oldObjects => {
+         const newDices = generateNewDices(diceAmount)
+         for (let i = 0; i < oldObjects.length; i++) {
+            oldObjects[i].isSelected && (newDices[i] = oldObjects[i])
+         }
+         return newDices
+      })
+   } else {
+      setDiceObjects(generateNewDices(diceAmount))
+   }
 }
 
 function toggleDice(setDiceObjects, id) {
    setDiceObjects(oldObjects => oldObjects.map(oldDice => {
       return oldDice.id === id
-         ? {...oldDice, isSelected: !oldDice.isSelected}
+         ? { ...oldDice, isSelected: !oldDice.isSelected }
          : oldDice
    }))
+}
+
+function didIWin(diceObjects, setTenzies) {
+
+   const firstDice = diceObjects[0]
+   const isAllSelected = diceObjects.every(dice => dice.isSelected)
+   const isAllSameValue = diceObjects.every(dice => dice.value === firstDice.value)
+   if(isAllSelected && isAllSameValue){
+      setTenzies(true)
+      return true
+   }
+   setTenzies(false)
+   return false
 }
 
 export default function App() {
 
    const [diceAmount, setDiceAmount] = React.useState(10)
    const [diceObjects, setDiceObjects] = React.useState(generateNewDices(diceAmount))
+   const [tenzies, setTenzies] = React.useState(false)
 
    const diceElements = diceObjects.map((dice) =>
       <Dice
@@ -50,27 +69,27 @@ export default function App() {
       />
    )
 
-   //Console Feedback
    React.useEffect(() => {
-      console.log(diceObjects)
+      didIWin(diceObjects, setTenzies)
    }, [diceObjects])
 
    return (
       <main>
-         <h1 className="title" >Tenzies</h1>
+         <h1 className="title" >{tenzies ? "YOU WON!!!!" : "Tenzies"}</h1>
          <p className="instructions">Roll until all dice are the same. Click each dice to freeze it as its current value between rolls</p>
          <div className="dice-container">
             {diceElements}
          </div>
          <button className="roll-dice"
-            onClick={(event) => { rollDice(event, diceAmount, setDiceObjects) }}
+            onClick={(event) => {rollDice(event, diceAmount, setDiceObjects, tenzies)}}
          >
-            Roll
+            {tenzies ? "New Game" : "Roll"}
+            {tenzies && <Confetti />}
          </button>
          <input
             placeholder={`Insert Dices Amount: ${diceAmount}`}
             onChange={(event) => {
-               setDiceAmount(event.target.value)
+               diceObjects.every(dice => !dice.isSelected) && setDiceAmount(event.target.value)
             }}
          />
       </main>
@@ -78,5 +97,8 @@ export default function App() {
 }
 
 /*
-
+   //Console Feedback
+   React.useEffect(() => {
+      console.log(diceObjects)
+   }, [diceObjects])
 */
