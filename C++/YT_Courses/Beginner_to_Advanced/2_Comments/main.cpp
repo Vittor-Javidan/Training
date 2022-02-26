03df3e3bd16a8531a5e8dc4a1e9dcfcf3b4470fb
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-02-26 11:30:31 -0300

    C++/YT_Courses/Beginner_to_Advanced: 2_Comments

---
+++ C++/YT_Courses/Beginner_to_Advanced/2_Comments/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+//Entry point main function
+int main(int argc, char **argv)
+{
+   //One line comment
+
+   /*
+      Multi-line block comment
+      Another line
+      Oh! And another line !
+   */
+
+  //Print out some text
+  std::cout << "Hello World in C++20!" << std::endl;
+  return 0;
+}
+
+
+//------------------------------------------------------------------------------------------------
+
+//This brings in the iostream library
+#include <iostream>
+
+/*
+   This is a block comment that englobes multiple
+   lines of text
+*/
+
+int main(){
+   //This is going to print "Hello World" to the console
+   std::cout << "Hellow World" << std::endl;
+   return 0;
+   //Program ends here
+}
