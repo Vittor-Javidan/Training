49ce577c73cc70e475de1cbd52d6c05010034340
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-02-26 11:47:43 -0300

    C++/YT_Courses/Beginner_to_Advanced: 3_Functions: more examples added

--- C++/YT_Courses/Beginner_to_Advanced/3_Functions/main.cpp
+++ C++/YT_Courses/Beginner_to_Advanced/3_Functions/main.cpp
@@ -21,3 +21,40 @@ int main(){
 
    return 0;
 }
+
+//------------------------------------------------------------------------------
+
+#include <iostream>
+
+int addNumbers( int first_param, int second_param ){
+
+   int result = first_param + second_param;
+   return result;
+}
+
+int mutiplyNumbers(int number_1, int number_2){
+   return number_1 * number_2;
+}
+
+int main(){
+
+   int first_number {3}; //statement
+   int second_number {7};
+
+   std::cout << "First number : " << first_number << std::endl;
+   std::cout << "Second number : " << second_number << std::endl;
+
+   int sum = first_number + second_number;
+   std::cout << "Sum : " << sum << std::endl;
+
+   sum = addNumbers(first_number, second_number);
+   std::cout << "Sum: " << sum << std::endl;
+
+   sum = addNumbers(30, 67);
+   std::cout << "Sum: " << sum << std::endl;
+   std::cout << "Sum: " << addNumbers(40, 34) << std::endl;
+
+   std::cout << "result: " << mutiplyNumbers(first_number, second_number) << std::endl;
+
+   return 0;
+}
