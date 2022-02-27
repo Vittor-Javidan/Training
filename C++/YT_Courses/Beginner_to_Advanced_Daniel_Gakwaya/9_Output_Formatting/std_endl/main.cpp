f7e2739a8835200995a3a58efe52bc20ed6c613e
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-02-27 20:12:10 -0300

    C++/YT_Courses/Beginner_to_Advanced_Daniel_Gakwaya: 9_Output_Formatting

---
+++ C++/YT_Courses/Beginner_to_Advanced_Daniel_Gakwaya/9_Output_Formatting/std_endl/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+   //sdt:endl
+
+   std::cout << "Hello";
+   std::cout << "World";
+
+   std::cout << std::endl;
+
+   std::cout << "Hello" << std::endl;
+   std::cout << "World" << std::endl;
+
+   std::cout << std::endl;
+
+   std::cout << "Hello\n";
+   std::cout << "World\n";
+
+   return 0;
+}
