c9bc0a0ce50ecfcaef305c3a5b5e1cf41c768f6c
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-02 17:00:57 -0300

    C++/YT_Courses/Beginner_to_Advanced_Daniel_Gakwaya: 16_Pass_by

---
+++ C++/YT_Courses/Beginner_to_Advanced_Daniel_Gakwaya/16_Pass_by/pass_by_reference/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void say_age(int &age);
+
+int main(){
+
+   /* Pass by reference means the function will reference to the variables you put as arguments */
+
+   int age = 23;
+
+   std::cout << "====================================================" << std::endl;
+
+   std::cout << "age - before : " << age << std::endl;   
+   std::cout << "age address - before : " << &age << std::endl;   
+   say_age(age);
+   std::cout << "age - after : " << age << std::endl;   
+   std::cout << "age address - after : " << &age << std::endl; 
+
+   std::cout << "====================================================" << std::endl;
+
+   return 0;
+}
+
+void say_age(int &age){
+
+   std::cout << "====================================================" << std::endl;
+
+   ++(age);
+   std::cout << "Hello! You are " << age << " years old." << std::endl;
+   std::cout << "age address - inside say_age : " << &age << std::endl; 
+
+   std::cout << "====================================================" << std::endl;
+}
