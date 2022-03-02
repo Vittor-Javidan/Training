ca08fedb5f60a61b9220231ce1f76a3a4ec8e285
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-02 16:03:10 -0300

    C++/YT_Courses/Beginner_to_Advanced_Daniel_Gakwaya: 15_Multiples_Files

---
+++ C++/YT_Courses/Beginner_to_Advanced_Daniel_Gakwaya/15_Multiples_Files/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "max.h"
+
+int main()
+{
+   std::cout << "max : " << max(34, 56) << std::endl;
+   std::cout << "min : " << min(34, 56) << std::endl;
+
+   return 0;
+}
