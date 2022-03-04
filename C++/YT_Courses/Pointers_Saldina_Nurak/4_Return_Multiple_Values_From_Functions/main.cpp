2b0039c3c2b9c708e424b48ecec9056bf10434c2
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-04 15:17:31 -0300

    C++/YT_Courses/Pointers_Saldina_Nurak/4_Return_Multiple_Values_From_Functions: getMinAndMax() now has automatic array lenght detection

--- C++/YT_Courses/Pointers_Saldina_Nurak/4_Return_Multiple_Values_From_Functions/main.cpp
+++ C++/YT_Courses/Pointers_Saldina_Nurak/4_Return_Multiple_Values_From_Functions/main.cpp
@@ -4,13 +4,13 @@
 using std::cout;
 using std::endl;
 
-void getMinAndMax(int numbers[], int numbers_Length,int* min, int* max){
+void getMinAndMax(int numbers_array[], int size,int* min, int* max){
 
-	for (int i = 0; i < numbers_Length; i++){
-		if(numbers[i] > *max)
-			*max = numbers[i];
-		if(numbers[i] < *min)
-			*min = numbers[i];
+	for (int i = 0; i < size; i++){
+		if(numbers_array[i] > *max)
+			*max = numbers_array[i];
+		if(numbers_array[i] < *min)
+			*min = numbers_array[i];
 	};
 };
 
@@ -18,11 +18,10 @@ int main(){
 	cout << "===========================================" << endl;
 
 	int numbers[5] = {5, 4, -2, 29, 6};
-	int numbers_Length = sizeof(numbers)/sizeof(numbers[0]);
 	int min = numbers[0];
 	int max = numbers[0];
 
-	getMinAndMax(numbers, numbers_Length, &min, &max);
+	getMinAndMax(numbers, sizeof(numbers)/4, &min, &max);
 	
 	cout << "Max number is: " << max << endl;
 	cout << "Min number is: " << min << endl;
