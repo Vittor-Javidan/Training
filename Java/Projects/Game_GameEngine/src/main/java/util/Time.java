4d3222b6940a8ca2ac9d896ff976118dff8856c1
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-08 17:43:17 -0300

    Java/Projects/Game_GameEngine: Time class removed, now time is aquired by glfwGetTime()

--- Java/Projects/Game_GameEngine/src/main/java/util/Time.java
+++
@@ -1,8 +0,0 @@
-package util;
-
-public class Time {
-    public static float timeStarted = System.nanoTime();
-
-    //Time since the application started
-    public static float getTime() {return  (float)((System.nanoTime() - timeStarted) * 1E-9); }
-}
