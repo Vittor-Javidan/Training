e884000fae97234ca37c2799428cf1c62b350702
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-09 23:04:17 -0300

    Java/Projects/Game_GameEngine: Alpha Blending and Z-indexing

--- Java/Projects/Game_GameEngine/src/main/java/jade/Window.java
+++ Java/Projects/Game_GameEngine/src/main/java/jade/Window.java
@@ -23,10 +23,10 @@ public class Window {
         this.width = 1920;
         this.height = 1080;
         this.title = "Mario";
-        r = 0;
-        g = 0;
-        b = 0;
-        a = 0;
+        r = 1;
+        g = 1;
+        b = 1;
+        a = 1;
     }
 
     public static void changeScene(int newScene){
@@ -110,6 +110,9 @@ public class Window {
         glfwShowWindow(glfwWindow);
 
         GL.createCapabilities();
+
+        glEnable(GL_BLEND);
+        glBlendFunc(GL_ONE, GL_ONE_MINUS_SRC_ALPHA);
 
         window.changeScene(0);
     }
