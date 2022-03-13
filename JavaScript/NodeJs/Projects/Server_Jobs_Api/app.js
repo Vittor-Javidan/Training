1fe1ec7955d4bd0972ca9125f3f6c3229ac118cb
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-12 23:41:08 -0300

    JavaScript/NodeJs/Projects/Server_Jobs_Api: Routers structure added

--- JavaScript/NodeJs/Projects/Server_Jobs_Api/app.js
+++ JavaScript/NodeJs/Projects/Server_Jobs_Api/app.js
@@ -3,6 +3,10 @@ require('express-async-errors')
 const express = require('express')
 const app = express();
 
+// routers
+const authRouter = require('./routes/auth')                                                                                                           // require auth Router from routes folder
+const jobsRouter = require('./routes/jobs')                                                                                                           // require jobs Router from routes folfer
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
@@ -11,9 +15,8 @@ app.use(express.json())
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-   res.send('jobs api')
-})
+app.use('/api/v1/auth', authRouter)                                                                                                                   // redirect all requests from '/api/v1/auth' to authRouter
+app.use('/api/v1/jobs', jobsRouter)                                                                                                                   // redirect all requests from '/api/v1/jobs' to jobsRouter
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
---
+++ JavaScript/NodeJs/Projects/Server_Jobs_Api/routes/jobs.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const jobsRouter = express.Router()
+
+const { getAllJobs, getJob, createJob, updateJob, deleteJob 
+} = require('../controllers/jobs')                                                                                                  // require all function from jobs controller
+
+jobsRouter.route('./')                                                                                                              // handles post and get requests wich cames from './' route
+   .post(createJob)                                                                                                                       // post request wich handles jobs creation
+   .get(getAllJobs)                                                                                                                       // get request wich handles jobs data requests
+
+jobsRouter.route('./:id')                                                                                                           // handles get, delete and patch requests wich cames from './:id' route
+   .get(getJob)                                                                                                                           // get request wich handles jobs data request from a single id
+   .delete(deleteJob)                                                                                                                     // delete request wich handles deletion of a job using an id
+   .patch(updateJob)                                                                                                                      // patch request wich handle update of a job using an id
+
+module.exports = jobsRouter
