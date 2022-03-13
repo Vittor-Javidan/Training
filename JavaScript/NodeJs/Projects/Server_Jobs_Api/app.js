42cea8fcd72a6c2cf512a270965e09fe28db57aa
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-13 20:30:47 -0300

    JavaScript/NodeJs/Projects/Server_Jobs_Api: just some comentaries organization.

--- JavaScript/NodeJs/Projects/Server_Jobs_Api/app.js
+++ JavaScript/NodeJs/Projects/Server_Jobs_Api/app.js
@@ -4,14 +4,14 @@ const express = require('express')
 const app = express();
 
 // connectDB
-const connectDB = require('./db/connect')                                                                                                             // require connectDB function to connect to database
+const connectDB = require('./db/connect')                                     // require connectDB function to connect to database
 
 // middleware
-const authenticateUser = require('./middleware/authentication')                                                                                       // require the authentication from our middlewares
+const authenticateUser = require('./middleware/authentication')               // require the authentication from our middlewares
 
 // routers
-const authRouter = require('./routes/auth')                                                                                                           // require auth Router from routes folder
-const jobsRouter = require('./routes/jobs')                                                                                                           // require jobs Router from routes folfer
+const authRouter = require('./routes/auth')                                   // require auth Router from routes folder
+const jobsRouter = require('./routes/jobs')                                   // require jobs Router from routes folfer
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found')
@@ -20,8 +20,8 @@ const errorHandlerMiddleware = require('./middleware/error-handler')
 app.use(express.json())
 
 // routes
-app.use('/api/v1/auth', authRouter)                                                                                                                   // redirect all requests from '/api/v1/auth' to authRouter
-app.use('/api/v1/jobs', authenticateUser, jobsRouter)                                                                                                 // redirect all requests from '/api/v1/jobs' to jobsRouter using our authenticateUser middleware
+app.use('/api/v1/auth', authRouter)                                           // redirect all requests from '/api/v1/auth' to authRouter
+app.use('/api/v1/jobs', authenticateUser, jobsRouter)                         // redirect all requests from '/api/v1/jobs' to jobsRouter using our authenticateUser middleware
 
 app.use(notFoundMiddleware)
 app.use(errorHandlerMiddleware)
@@ -31,7 +31,7 @@ const port = process.env.PORT || 3000
 const start = async () => {
 
    try {
-      await connectDB(process.env.MONGO_URI)                                                                                                          // connects to database using MONGO_URI, which is inside the file .env
+      await connectDB(process.env.MONGO_URI)                                        // connects to database using MONGO_URI, which is inside the file .env
       app.listen(port, () => {
          console.log(`Server is listening on port ${port}...`)
       })
--- JavaScript/NodeJs/Projects/Server_Jobs_Api/routes/auth.js
+++ JavaScript/NodeJs/Projects/Server_Jobs_Api/routes/auth.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const authRouter = express.Router()
 
-const { login, register } = require('../controllers/auth')                                                                                   // Imports login and register function from controlers
+const { login, register } = require('../controllers/auth')           // Imports login and register function from controlers
 
-authRouter.post('/register', register)                                                                                                       // handles post requests from "/register" route
-authRouter.post('/login', login)                                                                                                             // handles post requests from "/login" route
+authRouter.post('/register', register)                               // handles post requests from "/register" route
+authRouter.post('/login', login)                                     // handles post requests from "/login" route
 
 module.exports = authRouter
