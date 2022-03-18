625c4cf5dc5cbeb9f1261de68a1bc89045b87e65
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-17 21:45:10 -0300

    C++/Courses/C++_Cherno: added commentaries for the folder How_C++_Works

--- C++/Courses/C++_Cherno/How_C++_Works/Main.cpp
+++ C++/Courses/C++_Cherno/How_C++_Works/Main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <iostream>																		// Imports iostream library
 
-void Log(const char* message);
+void Log(const char* message);														// Declaration of the function "Log"
 
-int main() {											
+int main() {																				// Main Function. If you don't return 0, it just assumes the main function is returning 0, but works only with the main function
 
-	Log("Hello World");
-	std::cin.get();
+	Log("Hello World");																		// Calls our function to print "Hello World"
+	std::cin.get();																			// Makes the code stops until the user press a key
 }
