311cefd374cd1c4bc3c30442262c8d14a4b30b85
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-22 12:37:55 -0300

    C++/Projects/Script_Benchmark_Chrome_Tracing: Profiler to log formatted information into a json file added

--- C++/Projects/Script_Benchmark_Chrome_Tracing/Main.cpp
+++ C++/Projects/Script_Benchmark_Chrome_Tracing/Main.cpp
@@ -2,6 +2,86 @@
 #include <string>
 #include <cmath>
 #include <chrono>
+#include <algorithm>
+#include <fstream>
+
+struct ProfileResult {
+
+	std::string Name;
+	long long Start, End;
+};
+
+struct InstrumentationSession {
+
+	std::string Name;
+};
+
+class Instrumentor {
+private:
+	InstrumentationSession* m_CurrentSession;
+	std::ofstream m_OutputStream;
+	int m_ProfileCount;
+
+public:
+	Instrumentor()
+		: m_CurrentSession(nullptr), m_ProfileCount(0)
+	{}
+
+	void BeginSession(const std::string& name, const std::string& filepath = "results.json") {
+
+		m_OutputStream.open(filepath);
+		WriteHeader();
+		m_CurrentSession = new InstrumentationSession(name);
+	}
+
+	void EndSession() {
+
+		WriteFooter();
+		m_OutputStream.close();
+		delete m_CurrentSession;
+		m_CurrentSession = nullptr;
+		m_ProfileCount = 0;
+	}
+
+	void WriteProfile(const ProfileResult& result) {
+
+		if (m_ProfileCount++ > 0)
+			m_OutputStream << ",";
+
+		std::string name = result.Name;
+		std::replace(name.begin(), name.end(), '"', '\'');
+
+		m_OutputStream << "{";
+		m_OutputStream << "\"cat\":\"function\",";
+		m_OutputStream << "\"dur\":" << (result.End - result.Start) << ',';
+		m_OutputStream << "\"name\":\"" << name << "\",";
+		m_OutputStream << "\"ph\":\"X\",";
+		m_OutputStream << "\"pid\":0,";
+		m_OutputStream << "\"tid\":0,";
+		m_OutputStream << "\"ts\":" << result.Start;
+		m_OutputStream << "}";
+
+		m_OutputStream.flush();
+	}
+
+	void WriteHeader() {
+
+		m_OutputStream << "{\"otherData\": {},\"traceEvents\":[";
+		m_OutputStream.flush();
+	}
+
+	void WriteFooter() {
+
+		m_OutputStream << "]}";
+		m_OutputStream.flush();
+	}
+
+	static Instrumentor& Get() {
+
+		static Instrumentor* instance = new Instrumentor();
+		return *instance;
+	}
+};
 
 class Timer {
 private:
