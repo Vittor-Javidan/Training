d7793e31ec6e5f73def7107fa0201c0f4f0696e9
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-22 11:14:22 -0300

    Commentary Fixing
    Fixing the weird disposition of the c++ coding inside GitHub, giving a weird view of the code.

--- C++/Courses/C++_Cherno/1_How_C++_Works/Main.cpp
+++ C++/Courses/C++_Cherno/1_How_C++_Works/Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>					// Imports iostream library
 
-void Log(const char* message);		// Declaration of the function "Log"
+void Log(const char* message);				// Declaration of the function "Log"
 
 int main() {						// Main Function. If you don't return 0, it just assumes the main function is returning 0, but works only with the main function
 
