2b09c6563a23472df008c89046b93b712d6208b1
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-22 11:11:29 -0300

    Commentary Fixing
    Fixing the weird disposition of the c++ coding inside GitHub, giving a weird view of the code.

--- C++/Courses/C++_Cherno/1_How_C++_Works/Log.cpp
+++ C++/Courses/C++_Cherno/1_How_C++_Works/Log.cpp
@@ -1,5 +1,5 @@
-#include <iostream>																			// Imports the library iostream
+#include <iostream>									// Imports the library iostream
 
-void Log(const char* message) {															// Definition of our function "Log"
-	std::cout << message << std::endl;														// The logic how or function works. In that case, just printing a simple message
+void Log(const char* message) {								// Definition of our function "Log"
+	std::cout << message << std::endl;							// The logic how or function works. In that case, just printing a simple message
 }
