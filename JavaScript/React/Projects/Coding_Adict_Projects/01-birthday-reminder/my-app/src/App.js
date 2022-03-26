815473fb01af1395e7c2e507cda300c9979080c5
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-26 02:02:52 -0300

    JavaScript/React/Projects/Coding_Adict_Projects/01-birthday-reminder: First Commit

---
+++ JavaScript/React/Projects/Coding_Adict_Projects/01-birthday-reminder/my-app/src/App.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react"
+import data from './Data'
+import List from './List'
+
+export default function App() {
+	return <h2>reminder project setup</h2>
+}
---
+++ JavaScript/React/Projects/Coding_Adict_Projects/01-birthday-reminder/my-app/src/List.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function List () {
+
+	return (
+		<h2>list component</h2>
+	)
+}
