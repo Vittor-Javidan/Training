ebab09047ec33d027d58e9634efa1b8064883752
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-27 23:38:48 -0300

    JavaScript/React/Projects/App_Meme_Generator/Meme_Generator_Version_3: Meme component don't exist anymore

--- JavaScript/React/Projects/App_Meme_Generator/Meme_Generator_Version_3/src/App.js
+++ JavaScript/React/Projects/App_Meme_Generator/Meme_Generator_Version_3/src/App.js
@@ -1,12 +1,72 @@
+import React from "react"
+
 import Header from './components/Header'
-import Meme from "./components/Meme"
+
+import handleTextElements from "./components/handlers/handleTextElements"
+import handleInputElements from "./components/handlers/handleInputElements"
+import handleFontChange from "./components/handlers/handleFontChange"
+
+import addInputElements from "./components/scripts/addInputElements"
+import getNewImage from "./components/scripts/getNewImage"
 
 export default function App() {
+
+   const [allMemes, setAllMemes] = React.useState([])
+   const [meme, setMeme] = React.useState(() => {
+      return {
+         randomImage: "http://i.imgflip.com/1bij.jpg",
+         textFont: `${40}px`,
+         memeText: []
+      }
+   })
+   const [inputElements, setInputElement] = React.useState([])
+   const [textElements, setTextElements] = React.useState([])
+
+   React.useEffect(() => {
+      fetch("https://api.imgflip.com/get_memes")
+         .then(res => res.json())
+         .then(data => setAllMemes(data.data.memes))
+   }, [])
+
+   React.useEffect(() => {     
+      handleTextElements(meme, setTextElements)
+      handleInputElements(meme, setMeme, setInputElement)
+   }, [meme])
    
    return (
       <div>
          <Header />
-         <Meme />
+         <main>
+            <div>
+               <input
+                  type="text"
+                  placeholder={`Next New Text Font: ${meme.textFont}`}
+                  onChange={(e)=> handleFontChange(e, setMeme)}
+               />
+
+               <div className="form">
+                  {inputElements}
+                  <button
+                     className="form--button"
+                     onClick={(e)=> addInputElements(e, meme, setMeme)}
+                  >
+                     New Text
+                  </button>
+                  <button
+                     className="form--button"
+                     onClick={(e)=> getNewImage(e, allMemes, setMeme)}
+                  >
+                     Get a new meme image 🖼️
+                  </button>
+               </div>
+            </div>
+            <div
+               className="meme"
+            >
+               <img src={meme.randomImage} className="meme--image" alt="" />
+               {textElements}
+            </div>
+         </main>
       </div>
    )
 }
