48e4fc88a39f28d9dd6017dd6067a9345e0e3ab7
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-27 23:28:30 -0300

    JavaScript/React/Projects/App_Meme_Generator/Meme_Generator_Version_3: now Header Component are divided by sub compnents

--- JavaScript/React/Projects/App_Meme_Generator/Meme_Generator_Version_3/app/src/components/Header.js
+++ JavaScript/React/Projects/App_Meme_Generator/Meme_Generator_Version_3/app/src/components/Header.js
@@ -1,17 +1,26 @@
 export default function Header() {
 
-   return (
+   const HeaderImage = () => (
+      <img
+         src="./images/components/Header/troll-face.png"
+         className="header--image"
+         alt=""
+      />
+   )
 
-      <div>
-         <header className="header">
-            <img
-               src="./images/components/Header/troll-face.png"
-               className="header--image"
-               alt=""
-            />
-            <h2 className="header--title">Meme Generator</h2>
-            <h4 className="header--project">React Course - Project 3</h4>
-         </header>
-      </div>
+   const HeaderTitle = () => (
+      <h2 className="header--title">Meme Generator</h2>
+   )
+
+   const ProjectIndex = () => (
+      <h4 className="header--project">React Course - Project 3</h4>
+   )
+
+   return (
+      <header className="header">
+         <HeaderImage />
+         <HeaderTitle />
+         <ProjectIndex />
+      </header>
    )
 }
