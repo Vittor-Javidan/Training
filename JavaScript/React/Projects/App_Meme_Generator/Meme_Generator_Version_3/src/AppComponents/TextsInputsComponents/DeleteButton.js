d5c63979912d7093177d48624b1c2b45d76c0e9a
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-03-29 19:03:49 -0300

    JavaScript/React/Projects/App_Meme_Generator/Meme_Generator_Version_3: just some aling code indent fix on TextsInputs sub components

--- JavaScript/React/Projects/App_Meme_Generator/Meme_Generator_Version_3/src/AppComponents/TextsInputsComponents/DeleteButton.js
+++ JavaScript/React/Projects/App_Meme_Generator/Meme_Generator_Version_3/src/AppComponents/TextsInputsComponents/DeleteButton.js
@@ -1,33 +1,33 @@
 import { useContext } from "react"
 import { AppContext } from "../../App"
 
-export default function DeleteButton({index}) {
+export default function DeleteButton({ index }) {
 
-    const {texts, setTexts} = useContext(AppContext)
- 
-    function deleteInput(e) {
-       e.stopPropagation()
-    
-       const id = Number(e.target.id)
-       let textArray = []
-       if(id + 1 === texts.length){
-          textArray = texts.filter((text, index) => index !== id)
-       } else {
-          textArray = texts
-          textArray[id] = ""
-       }
-    
-       setTexts([...textArray])
-    }
- 
-    return (
-       <button
-          id={index}
-          type="reset"
-          className="form--input--delete-btn"
-          onClick={deleteInput}
-       >
-          <i className="form--input--gg-trash form--input--trash-icon" id={index}></i>
-       </button>
-    )
- }
+   const { texts, setTexts } = useContext(AppContext)
+
+   function deleteInput(e) {
+      e.stopPropagation()
+
+      const id = Number(e.target.id)
+      let textArray = []
+      if (id + 1 === texts.length) {
+         textArray = texts.filter((text, index) => index !== id)
+      } else {
+         textArray = texts
+         textArray[id] = ""
+      }
+
+      setTexts([...textArray])
+   }
+
+   return (
+      <button
+         id={index}
+         type="reset"
+         className="form--input--delete-btn"
+         onClick={deleteInput}
+      >
+         <i className="form--input--gg-trash form--input--trash-icon" id={index}></i>
+      </button>
+   )
+}
--- JavaScript/React/Projects/App_Meme_Generator/Meme_Generator_Version_3/src/AppComponents/TextsInputsComponents/TextInput.js
+++ JavaScript/React/Projects/App_Meme_Generator/Meme_Generator_Version_3/src/AppComponents/TextsInputsComponents/TextInput.js
@@ -1,27 +1,27 @@
 import { useContext } from "react"
 import { AppContext } from "../../App"
 
-export default function TextInput ({text, index}){
+export default function TextInput({ text, index }) {
 
-    const {texts, setTexts} = useContext(AppContext)
- 
-    function TextChange(e) {
- 
-       const { id, value } = e.target
-       let textArray = texts
-       textArray[id] = value
-    
-       setTexts([...textArray])
-    }
- 
-    return (
-       <input
-          className="form--input--text"
-          type="text"
-          id={index}
-          placeholder={`Text ${index + 1}`}
-          value={text}
-          onChange={TextChange}
-       />
-    )
- }
+   const { texts, setTexts } = useContext(AppContext)
+
+   function TextChange(e) {
+
+      const { id, value } = e.target
+      let textArray = texts
+      textArray[id] = value
+
+      setTexts([...textArray])
+   }
+
+   return (
+      <input
+         className="form--input--text"
+         type="text"
+         id={index}
+         placeholder={`Text ${index + 1}`}
+         value={text}
+         onChange={TextChange}
+      />
+   )
+}
